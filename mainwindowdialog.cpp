f4899f6e710aa9a6cdbf4f4f533a1bd247badd92
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-01-15 15:26:56 +0300

    dialog в принципе работает

--- mainwindowdialog.cpp
+++ mainwindowdialog.cpp
@@ -21,6 +21,7 @@ MainWindowDialog::MainWindowDialog(Cfg* cfg)
   QGridLayout* gl = new QGridLayout;
   QVector<Cfg::DialogEntry>* setup =
       static_cast<QVector<Cfg::DialogEntry>*>(cfg->Setup());
+  setWindowTitle(" ");  // Заголовок пустой для чистоты UI.
   int row = 0;
   for (const auto& a : *setup) {
     const auto type = a.type;
@@ -52,6 +53,39 @@ MainWindowDialog::MainWindowDialog(Cfg* cfg)
     gl->addWidget(gb, row, 1);
     ++row;
   }
+  QPushButton* b = new QPushButton("OK");
+  gl->addWidget(b, row, 0, 1, 2);
+  connect(b, &QPushButton::clicked, [gl]() {
+    QVector<QPair<QString, QStringList>> results;
+    for (int i = 0; i < gl->count() - 1; ++++i) {
+      QString title = qobject_cast<QLabel*>(gl->itemAt(i)->widget())->text();
+      QGroupBox* gb = qobject_cast<QGroupBox*>(gl->itemAt(i + 1)->widget());
+      QHBoxLayout* hbl = qobject_cast<QHBoxLayout*>(gb->layout());
+      QStringList values;
+      for (int j = 0; j < hbl->count(); ++j) {
+        QWidget* w = hbl->itemAt(j)->widget();
+        if (w != nullptr) {
+          if (QLineEdit* le = qobject_cast<QLineEdit*>(w)) {
+            values.append(le->text());
+          } else if (QRadioButton* rb = qobject_cast<QRadioButton*>(w)) {
+            if (rb->isChecked()) values.append(rb->text());
+          } else if (QCheckBox* cb = qobject_cast<QCheckBox*>(w)) {
+            if (cb->isChecked()) values.append(cb->text());
+          } else if (QPushButton* pb = qobject_cast<QPushButton*>(w)) {
+            values.append(pb->text());
+          }
+        }
+      }
+      results.append(qMakePair(title, values));
+    }
+
+    for (auto result : results) {
+      QString title = result.first;
+      QStringList values = result.second;
+      qInfo() << title + "=(" + values.join(" ") + ")";
+    }
+  });
+
   QWidget* w = new QWidget;
   w->setLayout(gl);
   this->setCentralWidget(w);
