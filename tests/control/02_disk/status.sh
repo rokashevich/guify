bbfa4416a739e1b842a44cfea83edc140b15c0e5
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-07-05 12:11:50 +0300

    Тестовый статус переменной длины

--- tests/control/02_disk/status.sh
+++ tests/control/02_disk/status.sh
@@ -1,9 +1,15 @@
 sleep 1
-t=7
+t=1
 n=1
 while true; do
-	echo '{"active":'$t', "label":"'$n'"}'
-	n=$((n+1))
 	((t^=1))
+	if [ $t -eq 1 ]
+	then
+		label=111111111111
+	else
+		label=0
+	fi
+	n=$((n+1))
+	echo '{"active":'$t', "label":"'$n$label'"}'
 	sleep 1
 done
